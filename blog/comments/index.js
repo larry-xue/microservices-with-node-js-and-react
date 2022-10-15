80ca20bd1b0563fee6e6a9698d8046aac2c715c9
Mercury-x <1612395322@qq.com>
2022-10-15 15:38:55 +0800

    react app complete

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
-const e = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -15,18 +16,16 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/posts/:id/comments', (req, res) => {
   const postId = req.params.id;
   const commentId = crypto.randomBytes(4).toString('hex');
-  const {
-    content
-  } = req.body;
+  const { content } = req.body;
 
-  const comments = commentsByPostId[postId] || []
+  const comments = commentsByPostId[postId] || [];
   comments.push({
     content,
-    id: commentId
-  })
+    id: commentId,
+  });
   commentsByPostId[postId] = comments;
 
-  return res.send(comments)
+  return res.send(comments);
 });
 
 app.listen(4001, () => {
