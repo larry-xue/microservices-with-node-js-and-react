f1fbf2ad56a66a27c7dbe824b776cfaf84f78dc8
Mercury-x <1612395322@qq.com>
2022-11-13 17:53:25 +0800

    feat: async errors

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -15,7 +16,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.get('*', () => {
+app.get('*', async () => {
   throw new NotFoundError();
 });
 
