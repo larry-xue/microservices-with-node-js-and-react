7df285886732ea470aceb04a423ee4aa698dc867
Mercury-x <1612395322@qq.com>
2022-11-15 13:10:42 +0800

    current user

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi!!!!!!!!!!!!!!!current user');
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!)
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
+  
 });
 
 export { router as currentUserRouter };
