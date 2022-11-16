82c031edbd8f07118febbcd6a9dd57d54d793f2c
Mercury-x <1612395322@qq.com>
2022-11-16 13:05:39 +0800

    feat: current user middleware

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,20 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../errors/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    res.send({ currentUser: null });
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({
+      currentUser: req.currentUser || null,
+    });
   }
-
-  try {
-    const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!)
-    res.send({ currentUser: payload })
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
-  
-});
+);
 
 export { router as currentUserRouter };
