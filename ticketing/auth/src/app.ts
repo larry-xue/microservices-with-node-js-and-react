8293f569688aab18fc13d69ce8fe7da535fd0c3b
Mercury-x <1612395322@qq.com>
2022-11-20 15:49:13 +0800

    cors

--- ticketing/auth/src/app.ts
+++ ticketing/auth/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
+import cors from 'cors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,6 +12,7 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.use(cors())
 app.use(json());
 app.set('trust proxy', true);
 app.use(
